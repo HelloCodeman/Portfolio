9d7d449dfc0dba8cf90df10960e7602f10a4aee5
HelloCodeman <955khm@gmail.com>
2024-03-23 16:46:39 +0800

    修改新增投票選項的選項排序/add_que縮排

--- ishot/api/add_que.php
+++ ishot/api/add_que.php
@@ -3,16 +3,34 @@ include_once "db.php";
 
 //判斷主題是否存在
 if (isset($_POST['subject'])) {
-    $Que->save(['text' => $_POST['subject'], 'subject_id' => 0, 'count' => 0]); //新增主題資料
-    $subject_id = $Que->find(['text' => $_POST['subject']])['id']; //根據剛才新增的主題資料去找到主題的資料id
-    // $subject_id = $Que->max('id'); //或是使用max()找到最大的id
+
+  //新增主題資料
+  $Que->save([
+    'text' => $_POST['subject'],
+    'subject_id' => 0,
+    'count' => 0
+  ]);
+
+  //根據剛才新增的主題資料去找到主題的資料id                
+  $subject_id = $Que->find(['text' => $_POST['subject']])['id'];
+
+  //或是使用max()找到最大的id
+  //$subject_id=$Que->max('id');
 }
 
 //判斷選項是否存在
 if (isset($_POST['option'])) {
-    foreach ($_POST['option'] as $option) {   //使用迴圈來巡訪 $_POST['option'] 陣列
-        $Que->save(['text' => $option, 'subject_id' => $subject_id, 'count' => 0]); //新增選項資料
-    }
+
+  //使用迴圈來巡訪 $_POST['option'] 陣列
+  foreach ($_POST['option'] as $option) {
+
+    //新增選項資料
+    $Que->save([
+      'text' => $option,
+      'subject_id' => $subject_id,
+      'count' => 0
+    ]);
+  }
 }
 
 to("../back/voteadmin.php");
--- ishot/back/voteadmin.php
+++ ishot/back/voteadmin.php
@@ -2,9 +2,9 @@
 include_once "../api/db.php";
 
 if (!isset($_SESSION['user'])) {
-    // 如果未登入，將用戶重新導向到index.php
-    echo "<script>alert('請先登入'); window.location.href = '../index.php';</script>";
-    exit;
+  // 如果未登入，將用戶重新導向到index.php
+  echo "<script>alert('請先登入'); window.location.href = '../index.php';</script>";
+  exit;
 }
 ?>
 
@@ -12,42 +12,42 @@ if (!isset($_SESSION['user'])) {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>投票後台</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="../js/js.js"></script>
-    <script src="../js/jquery-3.4.1.min.js"></script>
-    <style>
-        body {
-            font-family: fantasy;
-            font-style: italic;
-        }
-    </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>投票後台</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="../js/js.js"></script>
+  <script src="../js/jquery-3.4.1.min.js"></script>
+  <style>
+    body {
+      font-family: fantasy;
+      font-style: italic;
+    }
+  </style>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top" id="top">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="../index.php">
-                <i class="fa-solid fa-camera-retro"></i>
-                &nbsp;iShot
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link" aria-current="page" href="../index.php">iHome</a></li>
-                    <li class="nav-item"><a class="nav-link" href="../back/mem.php">iMember</a></li>
-                    <li class="nav-item"><a class="nav-link" href="../back/voteidx.php">iVote</a></li>
-                    <li class="nav-item"><a class="nav-link" href="../back/admin.php">iAdmin</a></li>
-                </ul>
-                <!-- <a class="btn btn-light" data-bs-toggle="offcanvas" href="#offcanvasExample" aria-controls="offcanvasExample">iLogin</a> -->
-            </div>
-        </div>
-    </nav>
-    <!-- <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top" id="top">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="../index.php">
+        <i class="fa-solid fa-camera-retro"></i>
+        &nbsp;iShot
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item"><a class="nav-link" aria-current="page" href="../index.php">iHome</a></li>
+          <li class="nav-item"><a class="nav-link" href="../back/mem.php">iMember</a></li>
+          <li class="nav-item"><a class="nav-link" href="../back/voteidx.php">iVote</a></li>
+          <li class="nav-item"><a class="nav-link" href="../back/admin.php">iAdmin</a></li>
+        </ul>
+        <!-- <a class="btn btn-light" data-bs-toggle="offcanvas" href="#offcanvasExample" aria-controls="offcanvasExample">iLogin</a> -->
+      </div>
+    </div>
+  </nav>
+  <!-- <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
         <div class="offcanvas-header">
             <h5>會員登入</h5>
         </div>
@@ -57,15 +57,15 @@ if (!isset($_SESSION['user'])) {
                     <div class="form-outline">
                         <?php
                         if (isset($_GET['error'])) {
-                            echo "<script>alert('帳號或密碼錯誤')</script>";
+                          echo "<script>alert('帳號或密碼錯誤')</script>";
                         }
                         ?>
                         <?php
                         if (isset($_SESSION['user'])) {
-                            echo "歡迎光臨 " . $_SESSION['user'];
-                            echo "&nbsp;";
-                            echo "<br>";
-                            echo "<a href='../api/logout.php' class='btn btn-dark' style='font-size:15px'>登出</a>";
+                          echo "歡迎光臨 " . $_SESSION['user'];
+                          echo "&nbsp;";
+                          echo "<br>";
+                          echo "<a href='../api/logout.php' class='btn btn-dark' style='font-size:15px'>登出</a>";
                         ?>
                         <?php
                         } else {
@@ -94,82 +94,82 @@ if (!isset($_SESSION['user'])) {
     </div>
     </div> -->
 
-    <header class="p-3">
-        <h1 class="text-center">投票主題管理</h1>
-    </header>
+  <header class="p-3">
+    <h1 class="text-center">投票主題管理</h1>
+  </header>
 
-    <main class="container">
-        <fieldset>
-            <legend style="text-align: center;">
-                新增投票
-            </legend>
-            <form action="../api/add_que.php" method="post">
-                <div class="d-flex">
-                    <div class="mx-auto">
-                        <!-- 主題 -->
-                        <div class="col bg-light p-2">
-                            <label for="subject">投票名稱&nbsp;</label>
-                            <input type="text" name="subject">
-                        </div>
-                        <!-- 選項 -->
-                        <div class="col bg-light p-2">
-                            <div id="opt">投票選項&nbsp;<!-- 給定id以便function定位 -->
-                                <input type="text" name="option[]">
-                                <input type="button" value="更多" onclick="more()">
-                            </div>
-                            <div class="mt-2 text-center">
-                                <input type="submit" value="新增">
-                                <input type="reset" value="清空">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </form>
-            <hr>
-            <legend style="text-align: center;">正在進行的投票</legend>
-            <div class="col-8 mx-auto">
-                <table class="table">
-                    <tr>
-                        <td>編號</td>
-                        <td>主題內容</td>
-                        <td>操作</td>
-                    </tr>
-                    <?php
-                    $ques = $Que->all(['subject_id' => 0]);
-                    foreach ($ques as $idx => $que) {
-                    ?>
-                        <tr>
-                            <td><?= $idx + 1; ?></td>
-                            <td><?= $que['text']; ?></td>
-                            <td>
-                                <!-- <a href="../api/show.php?id=<?= $que['id']; ?>" class="btn <?= ($que['display'] == 1) ? 'btn-info' : 'btn-secondary'; ?>">
+  <main class="container">
+    <fieldset>
+      <legend style="text-align: center;">
+        新增投票
+      </legend>
+      <form action="../api/add_que.php" method="post">
+        <div class="d-flex">
+          <div class="mx-auto">
+            <!-- 主題 -->
+            <div class="col bg-light p-2">
+              <label for="subject">投票名稱&nbsp;</label>
+              <input type="text" name="subject">
+            </div>
+            <!-- 選項 -->
+            <div class="col bg-light p-2">
+              <div id="opt">投票選項&nbsp;<!-- 給定id以便function定位 -->
+                <input type="text" name="option[]">
+                <input type="button" value="更多" onclick="more()">
+              </div>
+            </div>
+            <div class="mt-2 text-center">
+              <input type="submit" value="新增">
+              <input type="reset" value="清空">
+            </div>
+          </div>
+        </div>
+      </form>
+      <hr>
+      <legend style="text-align: center;">正在進行的投票</legend>
+      <div class="col-8 mx-auto">
+        <table class="table">
+          <tr>
+            <td>編號</td>
+            <td>主題內容</td>
+            <td>操作</td>
+          </tr>
+          <?php
+          $ques = $Que->all(['subject_id' => 0]);
+          foreach ($ques as $idx => $que) {
+          ?>
+            <tr>
+              <td><?= $idx + 1; ?></td>
+              <td><?= $que['text']; ?></td>
+              <td>
+                <!-- <a href="../api/show.php?id=<?= $que['id']; ?>" class="btn <?= ($que['display'] == 1) ? 'btn-info' : 'btn-secondary'; ?>">
                                     <?= ($que['display'] == 1) ? '顯示' : '隱藏'; ?>
                                 </a> -->
-                                <!-- <button class="btn btn-success">編輯</button> -->
-                                <a href="../api/del.php?id=<?= $que['id']; ?>">
-                                    <button class="btn btn-danger">刪除</button>
-                                </a>
-                            </td>
-                        </tr>
-                    <?php
-                    }
-                    ?>
-                </table>
-            </div>
-        </fieldset>
-    </main>
-    <script src="../js/jquery-3.4.1.min.js"></script>
-    <script src="../js/bootstrap.js"></script>
+                <a href="../api/del.php?id=<?= $que['id']; ?>">
+                  <button class="btn btn-danger">刪除</button>
+                </a>
+              </td>
+            </tr>
+          <?php
+          }
+          ?>
+        </table>
+      </div>
+    </fieldset>
+  </main>
 
-    <script>
-        function more() {
-            let opt = `
-                    <div id="opt">投票選項&nbsp;
+  <script src="../js/jquery-3.4.1.min.js"></script>
+  <script src="../js/bootstrap.js"></script>
+
+  <script>
+    function more() {
+      let opt = `
+                    <div>投票選項&nbsp;
                         <input type="text" name="option[]">
                     </div>`
-            $("#opt").before(opt)
-        }
-    </script>
+      $("#opt").before(opt)
+    }
+  </script>
 </body>
 
 </html>
